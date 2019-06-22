1ad59c13e1eac17994683b0927814bbc432f0aab
Alexey <nine-4lex@mail.ru>
2019-06-22 14:48:29 +0300

    [soft] joystick added

--- Software_Cinema/Conection.py
+++ Software_Cinema/Conection.py
@@ -3,13 +3,15 @@ import time
 import cv2
 import numpy as np
 from threading import Thread
+import math as m
 
 isActive = True
 
 class SerialTransfer(object):
 
     def __init__(self, num_port, speed=115200):
-        self.ser = serial.Serial(num_port, speed)
+        # self.ser = serial.Serial(num_port, speed)
+        pass
 
     def Send_pkg(self, num_motor, direction, value_of_step):
         pkg = bytes([ord('#'), num_motor, direction]) + \
@@ -18,14 +20,44 @@ class SerialTransfer(object):
         print(pkg)
         self.ser.write(pkg)
 
-    def getDebugLine(self):
-        return self.ser.readline()
+    def send_delays(self, hrz_delay, vrt_delay):
+        pkg = bytes([ord('#'), 
+                        np.int8(hrz_delay), 
+                        np.int8(vrt_delay), 
+                        np.uint8(hrz_delay * 2 + vrt_delay * 1.5)
+                    ])
+        print(pkg)
+        # self.ser.write(pkg)
 
+    def getDebugLine(self):
+        return self.ser.readline(100)
+
+def getDelayValue(norm_input):
+    # norm_input = [-1;1]
+    sign = lambda x: m.copysign(1, x)
+    return sign(norm_input) * 20
 
 if __name__ == "__main__":
 
+    serial_disabled = False
+    joystick_disabled = False
+
     import pygame
     pygame.init()
+
+    hrz_value_delay = 0
+    vrt_value_delay = 0
+        
+    try:
+        joystick = pygame.joystick.Joystick(0)
+        joystick.init()
+
+        joy_vrt_axis = 4
+        joy_hrz_axis = 0
+
+    except:
+        joystick_disabled = True
+
     pygame.display.set_caption("OpenCV camera stream on Pygame")
     screen = pygame.display.set_mode([640, 480])
 
@@ -44,16 +76,29 @@ if __name__ == "__main__":
 
     step_value = 10
 
-    MessageForSTM = SerialTransfer(num_port)
+    try:
+        MessageForSTM = SerialTransfer(num_port)
 
-    # Thread dependent
-    def prescript(serial_obj):
+        # Thread dependent
+        def debug_thread(serial_obj):
+            while isActive:
+                debug_line = serial_obj.getDebugLine()
+                print('Debug line: {}'.format(debug_line.rstrip(b'\n\r')))
+        thread_debug = Thread(target=debug_thread, args=[MessageForSTM])
+        thread_debug.start()
+    except:
+        serial_disabled = True
+        MessageForSTM = None
+            
+    def send_thread(serial_obj):
         while isActive:
-            debug_line = serial_obj.getDebugLine()
-            print('Debug line: {}'.format(debug_line.rstrip(b'\n\r')))
+            # print('{} / {}'.format(hrz_value_delay, vrt_value_delay))
+            time.sleep(0.2)
+            if serial_obj is not None:
+                serial_obj.send_delays(hrz_value_delay, vrt_value_delay)
 
-    thread_debug = Thread(target=prescript, args=[MessageForSTM])
-    thread_debug.start()
+    thread_send = Thread(target=send_thread, args=[MessageForSTM])
+    thread_send.start()
 
     print('Start')
     
@@ -73,23 +118,48 @@ if __name__ == "__main__":
                     pygame.quit()
                     isActive = False
 
-                keys_pressed = pygame.key.get_pressed()
+                # keys_pressed = pygame.key.get_pressed()
 
-                if keys_pressed[pygame.K_DOWN]:
-                    print('Down')
-                    MessageForSTM.Send_pkg(vrt_motor, down_direction, step_value)
+                # if keys_pressed[pygame.K_DOWN]:
+                #     print('Down')
+                #     if not serial_disabled:
+                #         MessageForSTM.Send_pkg(vrt_motor, down_direction, step_value)
 
-                if keys_pressed[pygame.K_UP]:
-                    print('Up')
-                    MessageForSTM.Send_pkg(vrt_motor, up_direction, step_value)
+                # if keys_pressed[pygame.K_UP]:
+                #     print('Up')
+                #     if not serial_disabled:
+                #         MessageForSTM.Send_pkg(vrt_motor, up_direction, step_value)
 
-                if keys_pressed[pygame.K_RIGHT]:
-                    print('Right')
-                    MessageForSTM.Send_pkg(hrz_motor, right_direction, step_value)
+                # if keys_pressed[pygame.K_RIGHT]:
+                #     print('Right')
+                #     if not serial_disabled:
+                #         MessageForSTM.Send_pkg(hrz_motor, right_direction, step_value)
 
-                if keys_pressed[pygame.K_LEFT]:
-                    print('Left')
-                    MessageForSTM.Send_pkg(hrz_motor, left_direction, step_value)
+                # if keys_pressed[pygame.K_LEFT]:
+                #     print('Left')
+                #     if not serial_disabled:
+                #         MessageForSTM.Send_pkg(hrz_motor, left_direction, step_value)
+
+                if not joystick_disabled:
+                    if event.type == pygame.JOYAXISMOTION:
+                        if event.axis == joy_hrz_axis:
+                            if abs(event.value) > 0.02: 
+                                hrz_value_delay = getDelayValue(event.value)
+                            else:
+                                hrz_value_delay = 0
+
+                        elif event.axis == joy_vrt_axis:
+                            if abs(event.value) > 0.02:
+                                vrt_value_delay = getDelayValue(-event.value)
+                            else:
+                                vrt_value_delay = 0
+                        
+                        # print(vrt_value_delay, hrz_value_delay)
+                            
+                    # if event.type == pygame.JOYBUTTONDOWN:
+                        # print(event.button)
+                    # elif event.type == pygame.JOYAXISMOTION:
+                        # print(event.axis, event.value)
 
     finally:
         pygame.quit()
